9121577a25666f979828d077a61842f28c91b82d
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-04-15 17:06:31 +0600

    Lunch page added

---
+++ src/Components/Foods/DisplayLunch/DIsplayLunch.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const DIsplayLunch = ({items}) => {
+    const { name, price, des, img } = items;
+    return (
+        <div>
+            <div className="flex flex-col justify-center items-center border-1 border-gray-200 m-5 p-10 hover:shadow-2xl rounded-lg">
+            <div>
+                <img width={"200px"} src={img} alt="" />
+            </div>
+            <div>
+                <h3 className="text-lg">{name}</h3>
+                <h4 className="text-gray-500">{des}</h4>
+                <p> ${price}</p>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default DIsplayLunch;
--- src/Components/Foods/Lunch/Lunch.js
+++ src/Components/Foods/Lunch/Lunch.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import lunch1 from '../../images/lunch/lunch1.png'
+import lunch2 from '../../images/lunch/lunch2.png'
+import lunch3 from '../../images/lunch/lunch3.png'
+import lunch4 from '../../images/lunch/lunch4.png'
+import lunch5 from '../../images/lunch/lunch5.png'
+import lunch6 from '../../images/lunch/lunch6.png'
+import DIsplayLunch from '../DisplayLunch/DIsplayLunch';
+
+
 const Lunch = () => {
+    const lunch = [
+        {id: 1, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: lunch1},
+        {id: 2, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: lunch2},
+        {id: 3, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: lunch3},
+        {id: 4, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: lunch4},
+        {id: 5, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: lunch5},
+        {id: 6, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: lunch6}
+    ]
     return (
-        <div>
-           <h1>This is lunch page.</h1> 
-        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 mx-10">
+        {
+            lunch.map(item => <DIsplayLunch
+            key={item.id}
+            items={item}
+            ></DIsplayLunch>)
+        }
+    </div>
     );
 };
 
