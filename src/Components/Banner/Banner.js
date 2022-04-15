c64c9d025a603eeb296f5e2866046f8314236df5
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-04-15 12:46:56 +0600

    Banner page somework done

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className="banner-container flex flex-col items-center justify-center">
+            <h1 className="text-5xl font-bold mb-4">Best food waiting for your belley</h1>
+             
+             <div className="">
+                 <input type="text" name="" id="" />
+                 <button>Search</button>
+             </div>
+        </div>
+    );
+};
+
+export default Banner;
