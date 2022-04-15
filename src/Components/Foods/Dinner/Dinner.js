916f50877e38639ca56213a868c386bf29e34a58
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-04-15 17:13:24 +0600

    dinner page added

--- src/Components/Foods/Dinner/Dinner.js
+++ src/Components/Foods/Dinner/Dinner.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import dinner1 from '../../images/dinner/dinner1.png'
+import dinner2 from '../../images/dinner/dinner2.png'
+import dinner3 from '../../images/dinner/dinner3.png'
+import dinner4 from '../../images/dinner/dinner4.png'
+import dinner5 from '../../images/dinner/dinner5.png'
+import dinner6 from '../../images/dinner/dinner6.png'
+import DisplayDinner from '../DisplayDinner/DisplayDinner';
 
 const Dinner = () => {
+    const breakfast = [
+        { id: 1, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: dinner1 },
+        { id: 2, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: dinner2 },
+        { id: 3, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: dinner3 },
+        { id: 4, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: dinner4 },
+        { id: 5, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: dinner5 },
+        { id: 6, name: "Bagel and cream chesse", des: "How we drem about our future", price: 6.99, img: dinner6 }
+    ]
     return (
-        <div>
-            <h1>This is dinner page</h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 mx-10">
+            {
+                breakfast.map(item => <DisplayDinner
+                    key={item.id}
+                    items={item}
+                ></DisplayDinner>)
+            }
         </div>
+
     );
 };
 
---
+++ src/Components/Foods/DisplayDinner/DisplayDinner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const DisplayDinner = ({items}) => {
+    const { name, price, des, img } = items;
+    return (
+        <div className="flex flex-col justify-center items-center border-1 border-gray-200 m-5 p-10 hover:shadow-2xl rounded-lg">
+        <div>
+            <img width={"200px"} src={img} alt="" />
+        </div>
+        <div>
+            <h3 className="text-lg">{name}</h3>
+            <h4 className="text-gray-500">{des}</h4>
+            <p> ${price}</p>
+        </div>
+    </div>
+    );
+};
+
+export default DisplayDinner;
